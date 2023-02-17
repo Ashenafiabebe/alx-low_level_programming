3c445bdd85035a54f97e1747cf67be08a5cddd33
Ashenafi <abebeashenafi7@gmail.com>
2023-02-17 12:00:08 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	int a;
+	int b;
+
+	for (a = '0'; b < '9'; a++)
+	{
+
+	for (b = a + 1; b <= '9'; b++);
+	{
+
+	if (a != b)
+	{
+	putchar(a);
+	putchar(b);
+
+	if (a == '8' && b == '9')
+
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
