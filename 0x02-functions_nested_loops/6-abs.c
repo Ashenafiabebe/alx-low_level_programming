11ab946d0c8036a3d887bab59615b82ff945a575
Ashenafi <abebeashenafi7@gmail.com>
2023-02-22 10:11:45 -0800

    100-time_table.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  *_abs - computes the absolute value of an integer
@@ -12,7 +13,7 @@ int _abs(int c)
 	if (c < 0)
 	{
 	int abs_val;
-	
+
 	abs_val = c * -1;
 	return (abs_val);
 	}
