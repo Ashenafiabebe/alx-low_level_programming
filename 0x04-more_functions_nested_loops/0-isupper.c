e70594352aec3a105e29f9f34d39e1694e8ba0a1
Ashenafi <abebeashenafi7@gmail.com>
2023-02-23 02:10:02 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if the letter is cappital
+ * @x: The number to be checked
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	if (x >= 65 && x <= 98)
+	{
+	return (1);
+	}
+	return (0);
+}
