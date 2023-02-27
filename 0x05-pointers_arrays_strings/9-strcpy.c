c3b3429c45407c556df4eae2fea5d95483391087
Ashenafi <abebeashenafi7@gmail.com>
2023-02-27 12:54:20 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* *_strcpy - will prints a string in reverse
+* @dest: destination value
+* @src: source value
+* Return: the pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+
+{
+int i;
+
+for (i = 0; src[i] != '\n'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
+}
