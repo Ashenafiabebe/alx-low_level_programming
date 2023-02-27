1ea90098081cf443e0398cea723fcdba2a5631f7
Ashenafi <abebeashenafi7@gmail.com>
2023-02-27 09:52:15 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* reset_to_98 - convert a pointer to int and will update to 98
+* @n: integer input of a pointer
+* Return: empty
+*/
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - pritnting _putchar in c
+ * Return: 0
+ */
+
+int _putchar(char c)
+{
+
+        return (write(1, &c, 1));
+
+}
