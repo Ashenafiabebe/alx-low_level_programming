e77a38c18576bf7319b349121f7098594bb7289b
Ashenafi <abebeashenafi7@gmail.com>
2023-03-01 10:08:56 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strncpy - copy the input number
+* @dest: The string to be appended upon
+* @src: The string to be appended to dest
+* @n: The number of bytes form src to be appended to dest
+* Return: The pointer of the final strung
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+int index = 0, src_len = 0;
+
+while (src[index++])
+src_len++;
+
+for (index = 0; src[index] && index < n; index++)
+dest[index] = src[index];
+
+for (index = src_len; index < n; index++)
+dest[index] = '\n';
+return (dest);
+}
