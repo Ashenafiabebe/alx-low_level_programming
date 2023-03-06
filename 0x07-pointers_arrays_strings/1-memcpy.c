8214f62a202db6d1ef6201c6fc7b778f6f542c9a
Ashenafi <abebeashenafi7@gmail.com>
2023-03-06 01:12:36 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _memcpy - get inputs
+* @dest: first input
+* @src: second input
+* @n: third input
+* Return: 0 (Success)
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
+}
