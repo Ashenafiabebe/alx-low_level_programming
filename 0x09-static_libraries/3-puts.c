bc0f4e5aa98c347d9ae7fce885dd9e735f63ae05
Ashenafi <abebeashenafi7@gmail.com>
2023-03-10 08:40:48 -0800

    libmy.a, main.h

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int puts(char *s)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
