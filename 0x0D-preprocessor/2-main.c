a8585b2a47dfe3e6e753f95d054181f2b3995b1c
Ashenafi <abebeashenafi7@gmail.com>
2023-03-23 23:36:36 -0700

    dog.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints the name of the file
+ *
  * Return: Always 0 (Success)
  */
-
 int ain(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
